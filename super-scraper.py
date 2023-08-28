2e1fac2f3e6a660b5376a486d4a72a755eb55a93
Owen Bowden (the404) <47672957+the404devs@users.noreply.github.com>
2023-08-28 13:39:00 -0400

    Update super-scraper.py

--- super-scraper.py
+++ super-scraper.py
@@ -7,7 +7,7 @@ from html import unescape
 
 def clean(text):
     if isinstance(text, str):
-        return unescape(text).strip().replace('\\','').replace('â', '-').replace('’', "'").replace("Ã¢ÂÂ", "-").replace("â", "'").replace("\\,", ",").replace("–","-")
+        return unescape(text).strip().replace('\\','').replace('â', '-').replace('’', "'").replace("Ã¢ÂÂ", "-").replace("â", "'").replace("\\,", ",").replace("–","-").replace("Ã©", "é")
     else:
         return text
 
